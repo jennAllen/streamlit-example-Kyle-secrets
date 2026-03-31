4ddd3334c478a430aad2fbe5d5bcd2bd81b88ca4
Jenn Allen <51343347+jennAllen@users.noreply.github.com>
2026-03-31 08:41:28 -0700

    Change default Snowflake database from JENN_MOVIES to DEMOS

--- movies_app.py
+++ movies_app.py
@@ -18,7 +18,7 @@ def init_connection():
         # Use environment variables with OAuth token authentication via Connect SDK
         account = os.environ["SNOWFLAKE_ACCOUNT"]
         warehouse = os.environ.get("SNOWFLAKE_WAREHOUSE", "DEFAULT_WH")
-        database = os.environ.get("SNOWFLAKE_DATABASE", "JENN_MOVIES")
+        database = os.environ.get("SNOWFLAKE_DATABASE", "DEMOS")
         schema = os.environ.get("SNOWFLAKE_SCHEMA", "PUBLIC")
 
         # Get user session token
